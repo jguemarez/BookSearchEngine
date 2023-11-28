d396ea96072484432d7f09c7ee73c9ca45f8db89
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-11-28 14:56:09 -0500

    Changing the options argument in the mongoose.connect method in order to see if I can deploy without errors to Heroku.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+/*const connectDB = async () => {
+    try {
+        await */
+        mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+   /* } catch (err) {
+        console.error('MongoDB connection error:', err);
+    }
+};
+
+connectDB()*/
 
 module.exports = mongoose.connection;
